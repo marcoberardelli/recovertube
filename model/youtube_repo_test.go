9192b90cf8b5da76bc272d9807f6decda489ce29
Marco Berardelli <marco.berardelli@gmail.com>
2021-11-17 17:58:40 +0100

    fixed first test

--- model/youtube_repo_test.go
+++ model/youtube_repo_test.go
@@ -1,52 +1,40 @@
 package model
 
 import (
-	"os"
 	"testing"
-	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func setup() (sqlmock.Sqlmock, error) {
-
-	videos := []Video{
-		{ID: "dQw4w9WgXcQ", Title: "Rick Astley - Never Gonna Give You Up (Official Music Video)", Channel: "UCuAXFkgsw1L7xaCfnd5JJOw", Available: true, LastUpdate: time.Now(), ImagePath: "images/dQw4w9WgXcQ"},
-		{ID: "9bZkp7q19f0", Title: "PSY - GANGNAM STYLE(강남스타일) M/V", Channel: "UCrDkAvwZum-UTjHmzDI2iIw", Available: true, LastUpdate: time.Now(), ImagePath: "images/9bZkp7q19f0"},
+func TestGetYTRepository(t *testing.T) {
+	_, errS := GetYTRepository()
+	if errS != ErrNoRepoInstance {
+		t.Fail()
 	}
-
-	err := os.Remove("test.db")
+	_db, _, err := sqlmock.New()
 	if err != nil {
-		return nil, err
+		t.Fail()
 	}
-
-	_db, mock, err := sqlmock.New()
 	dialector := postgres.New(postgres.Config{
 		Conn:       _db,
 		DriverName: "postgres",
 	})
-	postresDB, err := gorm.Open(dialector, &gorm.Config{})
+	db, err := gorm.Open(dialector, &gorm.Config{PrepareStmt: false})
 	if err != nil {
-		return mock, err
-	}
-	ytRepo = YoutTubeDBRepository{db: postresDB}
-	return mock, nil
-}
-
-func TestGetYTRepository(t *testing.T) {
-	_, err := GetYTRepository()
-	if err != ErrNoRepoInstance {
 		t.Fail()
 	}
-
-	setup()
+	ytRepo = YoutTubeDBRepository{db: db}
 
 	_, err = GetYTRepository()
 	if err != nil {
 		t.Fail()
 	}
+}
+
+func TestAddVideo(t *testing.T) {
+
 }
 
 func TestGetVideo(t *testing.T) {
